11ec28aeb9586c0d08b681971fce94667706b65e
Steve Womack <swomack@pivotal.io>
2022-02-18 15:54:47 -0600

    Rounds sensor readings

--- src/main/java/org/tanzu/demo/SensorsUiController.java
+++ src/main/java/org/tanzu/demo/SensorsUiController.java
@@ -18,7 +18,7 @@ public class SensorsUiController {
     }
 
     @GetMapping
-    public String fetchUI(Model model) throws Exception {
+    public String fetchUI(Model model) {
         var formattedSensorData = sensorRepository.findAll()
                 .stream().map(s -> new SensorData(
                                 s.getId(),
@@ -26,6 +26,7 @@ public class SensorsUiController {
                                 Math.round(s.getPressure() * 100) / 100.0d
                         )
                 ).collect(java.util.stream.Collectors.toList());
+        model.addAttribute("sensors", formattedSensorData);
         model.addAttribute("title", title);
         return "index";
     }
